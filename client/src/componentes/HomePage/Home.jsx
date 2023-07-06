bc46afc1c29529e29ba9748553a8354a2fecef6e
Galbfran <francogalbiati984@gmail.com>
2023-07-06 13:01:04 -0300

    subo paginado

--- client/src/componentes/HomePage/Home.jsx
+++ client/src/componentes/HomePage/Home.jsx
@@ -1,6 +1,6 @@
 //Hooks
 import { useDispatch , useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect , useState} from "react";
 
 
 //redux-action
@@ -10,34 +10,65 @@ import { getAllDogs } from "../../redux/actions";
 import NavBar from "../NavBar";
 import CardDog from "../Dogs/CardDog";
 //css
-
+import styles from './Home.module.css'
 
 const Home = () => {
+    const ITEM_PAGE = 8
+
     const dispatch = useDispatch();
-    const dogsRender = useSelector(state => state.allDogs)
-    console.log(dogsRender)
+    const [ dogs , setDogs] = useState(useSelector(state => state.allDogs))
+    
     useEffect(() => {
-        dispatch(getAllDogs())
+        dispatch(getAllDogs());
     }, [])
+    
+    const [dogsRender , setDogsRender] = useState([...dogs].splice(0 , ITEM_PAGE));
+    const [ currentPage , setCurrentPage] = useState(1);
 
+    const nextHandler = () => {
+        const totalElement = dogs.length;
+        const nextPage = currentPage + 1 ;
+        const firstIndex = nextPage * ITEM_PAGE;
+        if (firstIndex >= totalElement) return;
+        setDogsRender([...dogs].splice(firstIndex , ITEM_PAGE));
+        setCurrentPage(nextPage);
+    }
 
+    const prevHandler = () => {
+        const prevPage = currentPage - 1;
+        if (prevPage < 0) return;
+        const firstIndex = prevPage * ITEM_PAGE;
+        setDogsRender([...dogs].splice(firstIndex , ITEM_PAGE));
+        setCurrentPage(prevPage);
+    }
 
     return(
+        <section>
+            <div>
+                <NavBar/>
+            </div>
+            <article >
+                <div>
+                    <p> Pagina Numero {currentPage +1}</p>
+                </div>
+                <div className={styles.container}>
+                {   dogsRender.map(dog => {
+                        return <CardDog 
+                            key={dog?.ID_Dogs}
+                            id={dog?.ID_Dogs}
+                            image={dog?.Imagen}
+                            name={dog?.Nombre}
+                            temperamento={dog?.Temperamento}
+                            peso={dog?.Peso}
+                        />})
+                }
+                </div>
+            </article>
         <div>
-            <NavBar/>
-        {    
-            dogsRender.map(dog => {
-                return <CardDog 
-                    key={dog?.ID_Dogs}
-                    id={dog?.ID_Dogs}
-                    image={dog?.Imagen}
-                    name={dog?.Nombre}
-                    temperamento={dog?.Temperamento}
-                    peso={dog?.Peso}
-                />
-                })
-            }
+            <button onClick={() => prevHandler()}>Anterior</button>
+            <button onClick={() => nextHandler()}>Siguente</button>
         </div>
+        </section>
     )
 }
 
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -4,7 +4,6 @@ import axios from 'axios'
 export const getAllDogs = () => {
     return async (dispatch) => {
         let response = await axios('http://localhost:3001/dogs/');
-        console.log(response.data + 'console action')
         return dispatch({ type: GET_DOGS , payload: response.data })//modificar despues para perros
     }
 }
